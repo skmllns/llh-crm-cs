17f12cabe3480c3adb00adec52e065ef65b23cf9
skmullins <skmllns@gmail.com>
2016-06-25 11:43:56 -0400

    Initial commit

---
+++ llh-crm-cs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="llh_crm_cs.MvcApplication" Language="C#" %>
---
+++ llh-crm-cs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(llh_crm_cs.Startup))]
+namespace llh_crm_cs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
