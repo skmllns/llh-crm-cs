0a478ce079721874ca0490d088e0ba21d4d8fa31
skmullins <skmllns@gmail.com>
2016-06-30 22:35:36 -0400

    Added Artist model

--- llh-crm-cs/Models/Artist.cs
+++ llh-crm-cs/Models/Artist.cs
@@ -1,7 +1,11 @@
-﻿namespace llh_crm_cs.Models
+﻿using System.Collections.Generic;
+
+namespace llh_crm_cs.Models
 {
     public class Artist
     {
         public int ArtistID { get; set; }
+        public string ArtistCategory { get; set; }
+        public virtual ICollection<Occurence> Occurences { get; set; }
     }
 }
